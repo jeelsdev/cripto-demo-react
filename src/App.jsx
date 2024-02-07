8d1a586183cd95212c23d77c774bdf9b1d920a36
Joels <jyoel.huaman@gmail.com>
2024-02-07 00:30:13 -0500

    basic fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [cript, setCript] = useState();
+
+  useEffect(()=>{
+    fetch("https://api.coincap.io/v2/assets")
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCript(data.data);
+     
+    })
+    .catch(()=>{
+      console.error("Failed to operation");
+    })
+  }, [])
+
+  if(!cript) return <span>cargando...</span>
 
   return (
     <>
-    <h2>Hello</h2>  
+     <h2>Lista de criptomonedas</h2>
+     <ol>
+      {cript.map(({name, priceUsd})=>(
+        <li>Name: {name} Price: {priceUsd}</li>
+      ))}
+     </ol>
     </>
   )
 }
